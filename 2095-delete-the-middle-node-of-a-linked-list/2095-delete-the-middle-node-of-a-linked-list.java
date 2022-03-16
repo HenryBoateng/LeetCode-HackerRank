b2945db8c717b7cb62ff50967c4d79009acb6279
Henry Boateng <Henry7107@hotmail.co.uk>
2022-03-16 07:35:14 +0000

    Time: 8 ms (12.02%), Space: 238.1 MB (11.04%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -11,51 +11,25 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
        
-//         ListNode curr = head;
-//         ListNode slow = head;
-//         ListNode fast = head;
-//         ListNode dummy = head;
-//         int count = 0;
-//         int countTwo = 0;
-        
-//         if (curr != null && curr.next == null){
-//             return curr;
-//         }
-        
-//         while(fast != null && fast.next != null){
-//             dummy=slow;
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-        
-        
-//         return dummy;
-//          if (dummy.next.val==slow.val){
-//                  dummy.next = dummy.next.next;
-//              }
-//          while (dummy!=null && dummy.next!=null){
-//              dummy=dummy.next;
-//          }
-        
-        if (head == null){
-            return null;
-        }
-        if (head.next == null){
-            return null;
-        }
-        
+        ListNode curr = head;
         ListNode slow = head;
         ListNode fast = head;
-        ListNode prev = null;
+        ListNode dummy = null;
+
+        if (curr != null && curr.next == null){
+            return null;
+        }
         
-        while (fast != null && fast.next !=null){
+        while(fast != null && fast.next != null){
             fast = fast.next.next;
-            prev = slow;
+            dummy=slow;
             slow = slow.next;
         }
         
-        prev.next = slow.next;
-            return head;
-            
+        dummy.next = slow.next;
+        return head;
+
+        
+
 }
 }
